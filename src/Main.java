bd1da85e0e8fb79be227b738d848304b26449520
MauDJ-MIM <md469153@students.mimuw.edu.pl>
2025-05-02 14:41:52 +0200

    Test commit - IO

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-import java.util.Scanner;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) extends IOException {
+        try {
+            File input = new File("input.txt");
+            Scanner inputScanner = new Scanner(input);
 
-        //Tworzymy scanner
-        //Służy on do tego, by wczytać komendę.
-        //Następnie na podstawie tej komendy zrobimy z plikiem to, co należy
-        Scanner scanner = new Scanner(System.in);
-        String komenda = scanner.nextLine();
+            //Deklarujemy nowe pliki
+            File encodeCezar = new File("encodeCezar.txt");
+            File decodeCezar = new File("decodeCezar.txt");
+            File encodeHuffman = new File("encodeHuffman.txt");
+            File decodeHuffman = new File("decodeHuffman.txt");
 
-        //Patrzymy jaka jest komenda.
-        //Na jej podtawie wykonujemy odpowiednią funkcję.
+            //Tworzymy nowe pliki.
+            //Jeśli są już pliki o danych nazwach, to je usuwamy i stwarzamy na nowo puste pliki.
+            if(!(encodeCezar.createNewFile())) {encodeCezar.delete(); encodeCezar.createNewFile();}
+            if(!(decodeCezar.createNewFile())) {decodeCezar.delete(); decodeCezar.createNewFile();}
+            if(!(encodeHuffman.createNewFile())) {encodeHuffman.delete(); encodeHuffman.createNewFile();}
+            if(!(encodeHuffman.createNewFile())) {encodeHuffman.delete(); encodeHuffman.createNewFile();}
 
+
+        } catch (IOException e) {
+            System.out.println("Błąd wejścia - wyjścia. \n");
+            e.printStackTrace();
+        }
     }
 }
